6a77b44c6c93ea85924bc499f3de0f5ea9e5c764
Lawon <86679952+Lawon562@users.noreply.github.com>
2023-03-21 09:37:57 +0900

    day2 lesson1()

--- day2_1/day2_1/Program.cs
+++ day2_1/day2_1/Program.cs
@@ -8,8 +8,25 @@ namespace day2_1
 {
     internal class Program
     {
+
+        public static void lesson1()
+        {
+            var q1 = ".NET Frmaework";
+            var q2 = 10;
+            var q3 = 123.567;
+            var q4 = '옹';
+            var q5 = false;
+
+            Console.WriteLine($"\t q1의 값은 {q1}, 데이타 형은 {q1.GetType()}");
+            Console.WriteLine($"\t q1의 값은 {q2}, 데이타 형은 {q2.GetType()}");
+            Console.WriteLine($"\t q1의 값은 {q3}, 데이타 형은 {q3.GetType()}");
+            Console.WriteLine($"\t q1의 값은 {q4}, 데이타 형은 {q4.GetType()}");
+            Console.WriteLine($"\t q1의 값은 {q5}, 데이타 형은 {q5.GetType()}");
+        }
+
         static void Main(string[] args)
         {
+            lesson1();
         }
     }
 }
