66bc05428140a86efef1b8de07f6b7fe6583136b
Lawon <86679952+Lawon562@users.noreply.github.com>
2023-03-21 10:15:07 +0900

    day2 lesson1_search_method

--- day2_1/day2_1/Program.cs
+++ day2_1/day2_1/Program.cs
@@ -32,7 +32,8 @@ namespace day2_1
             float p3 = default;
             bool p4 = default;
             char p5 = default;
-            string p6 = default;   // 오류 발생 
+            //string p6 = default;   // 오류 발생 
+            string p6 = "";     
 
             Console.WriteLine($"\t p1의 값은 {p1} 데이타형은 {p1.GetType()}");
             Console.WriteLine($"\t p2의 값은 {p2} 데이타형은 {p2.GetType()}");
@@ -42,11 +43,29 @@ namespace day2_1
             Console.WriteLine($"\t p6의 값은 {p6} 데이타형은 {p6.GetType()}");
         }
 
+        public static void lesson3_search_method()
+        {
+            string sampleTxt1 = "Good Morning";
+            string sampleTxt2 = "가나다라마바사아자차카타파하";
+            Console.WriteLine($"sampleTxt1 = {sampleTxt1}");
+            Console.WriteLine($"sampleTxt2 = {sampleTxt2}");
+            Console.WriteLine($"마지막 o의 인덱스 위치 = {sampleTxt1.LastIndexOf("o")}");
+            Console.WriteLine($"a로 시작하는가? {sampleTxt1.StartsWith("a")}");
+            Console.WriteLine($"G로 시작하는가? {sampleTxt1.StartsWith("G")}");
+            Console.WriteLine($"G로 끝나는가? {sampleTxt1.EndsWith("a")}");
+            Console.WriteLine($"ing로 끝나는가? {sampleTxt1.EndsWith("ing")}");
+            Console.WriteLine($"차가 포함되었는가? {sampleTxt1.Contains("차")}");
+            Console.WriteLine($"Morning 글자 교체 {sampleTxt1.Replace("Morning", "Night")}");
+
+        }
+
         static void Main(string[] args)
         {
             //lesson1();
 
-            lesson1_default();
+            //lesson1_default();
+
+            lesson3_search_method();
         }
     }
 }
