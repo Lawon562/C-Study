8aa208bf6f2371beaf6a706d978f8c88fbc4cc8f
Lawon <86679952+Lawon562@users.noreply.github.com>
2023-03-21 17:49:34 +0900

    day2_2 operator

--- day2_2/day2_2/Program.cs
+++ day2_2/day2_2/Program.cs
@@ -8,6 +8,61 @@ namespace day2_2
 {
     internal class Program
     {
+        public static string bar = "----------------------------------------------------";
+        public static void quizTitle()
+        {
+            Console.WriteLine($"\n\n{bar}");
+            for (int i = 0; i < bar.Length / 2; i++)
+            {
+                Console.Write(" ");
+            }
+            Console.WriteLine("퀴즈");
+            Console.WriteLine($"{bar}");
+        }
+
+        public static void discription(string[] args)
+        {
+            for (int i = 0; i < args.Length; i++)
+            {
+                Console.Write("· ");
+                for (int k = 0; k < args[i].Length; k++)
+                {
+                    if (k % (bar.Length / 2) == bar.Length / 2 - 1)
+                    {
+                        Console.WriteLine("");
+                        Console.Write("   ");
+                    }
+                    Console.Write($"{args[i][k]}");
+                    
+                }
+                Console.WriteLine("\n");
+                
+            }            
+        }
+
+        public static void printScreen()
+        {
+            Console.WriteLine("\n----------------------------");
+            Console.WriteLine("          출력화면");
+            Console.WriteLine("----------------------------\n");
+
+        }
+
+        public static void Quiz(string[] args)
+        {
+            quizTitle();
+            discription(args);
+
+            printScreen();
+        }
+
+        public static int GetInteger(string args)
+        {
+            Console.Write($"{args}");
+            int number = Convert.ToInt32(Console.ReadLine());
+            return number;
+        }
+
         static void Main(string[] args)
         {
             //double n1 = 100;
@@ -86,9 +141,71 @@ namespace day2_2
             Console.WriteLine($"\t 7단계 : n = {--n}");
             */
 
-            Console.Write("비밀번호 입력 => ");
-            string pwd = Console.ReadLine();
-            Console.WriteLine($"결과 : {pwd=="1234"}");
+            //Console.Write("비밀번호 입력 => ");
+            //string pwd = Console.ReadLine();
+            //Console.WriteLine($"결과 : {pwd=="1234"}");
+
+            //Console.Write("철수의 나이는?  ... ");
+            //int age = Convert.ToInt32(Console.ReadLine());
+
+            //Console.Write("\n철수의 키는?  ... ");
+            //int height = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("----------------------------");
+            //Console.WriteLine("          출력화면");
+            //Console.WriteLine("----------------------------");
+
+            //Console.WriteLine($"철수는 놀이기구를 탈 수 있을까요? {age >= 10 && height >= 150}");
+
+            //Console.Write("철수의 쿠폰 -> ");
+            //int couponA = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("영희의 쿠폰 -> ");
+            //int couponB = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("결과 >> ");
+            //Console.WriteLine($"철수와 영희는 치킨을 먹을 수 있을까... ? {couponA + couponB >= 10}");
+
+            Quiz(new string[]
+            {
+                "3개의 숫자를 변수로 정의하고 3숫자의 합이 500보다 크고 3숫자 모두 짝수이면 True 그렇지 않으면 False를 출력하도록 프로그래밍 하여라"
+            });
+
+            int num1 = GetInteger("첫 번째 수 : ");
+            int num2 = GetInteger("두 번째 수 : ");
+            int num3 = GetInteger("세 번째 수 : ");
+
+            bool res1 = num1 + num2 + num3 > 500;
+            bool res2 = num1 % 2 == 0 && num2 % 2 == 0 && num3 % 2 == 0;
+            Console.WriteLine($"세 수의 합은 500보다 큰가? {res1}");
+            Console.WriteLine($"세 수는 모두 짝수인가? {res2}");
+            Console.WriteLine($"세 수의 합은 500보다 크고 세 수는 모두 짝수인가?? {res1 && res2}");
+
+
+            Quiz(new string[] 
+            { 
+                "조건 연산자를 이용하여 짝수인지 홀수인지 3과 7의 배수인지를 아래와 같은 결과 화면으로 출력하여라.",
+                "입력 받은 숫자를 이용하여야 한다."
+            });
+
+            int number = GetInteger("\n숫자를 입력하여 주세요 ... ");
+            string result = number % 2 == 0 ? "짝수" : "홀수";
+            Console.WriteLine($"{number}는 {result}");
+            Console.WriteLine(number % 3 == 0 && number % 7 == 0 ? $"{number}는 3과 7의 배수" : "");
+
+            Quiz(new string[]
+            {
+                "철수는 놀이공원에서 놀이기구를 타려고 한다. 놀이 기구는 키가 150cm 이상이어야 하고 나이가 10세 이상이어야 한다.",
+                "철수의 나이와 키는 입력 받는 값을 이용한다.",
+                "조건연산자를 이용하여 철수가 놀이기구를 탈 수 있을지 메세지로 출력한다.",
+                "(탈 수 있다 | 탈 수 없다)"
+            });
+
+            int age = GetInteger("철수의 나이는? ... ");
+            int height = GetInteger("철수의 키는? ... ");
+            
+            result = height >= 150 || age >= 10 ? "탈 수 있다." : "탈 수 없다";
+            Console.WriteLine($"철수는 놀이기구를 탈 수 있을까요? {result}");
+
         }
     }
 }
