80e41db44ebe1bc3f9d03b297fbd51900841226c
Lawon <86679952+Lawon562@users.noreply.github.com>
2023-04-04 10:24:19 +0900

    0404init

---
+++ BackJoon/BackJoon/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackJoon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = 0;
+            while ((N = int.Parse(Console.ReadLine())) != -1)
+            {
+                int sum = 0;
+
+                List<int> list = new List<int>();
+
+                for (int i = 1; i <= N / 2; i++)
+                {
+                    if (N % i == 0)
+                    {
+                        list.Add(i);
+                        sum += i;
+                    }
+                }
+
+
+                if (sum == N)
+                {
+                    Console.WriteLine($"{N} = {string.Join(" + ", list)}");
+                }
+                else
+                {
+                    Console.WriteLine($"{N} is NOT perfect");
+                }
+
+            }
+        }
+    }
+}
